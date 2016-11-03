0d7ec6d1c6303b5adb5d42a0f533a227b0a39cce
jtemple10 <john.temple@rogers.com>
2016-11-03 10:26:36 -0400

    Schedule Class
    needs new package name

---
+++ Schedule.java
@@ -0,0 +1,29 @@
+
+package ooadass1;
+
+import java.util.*;
+
+/**
+ *
+ * @author John
+ */
+public class Schedule {
+    
+    List<Exam> examList = new ArrayList<Exam>();
+    
+    
+    public Schedule(List<Exam> loe)
+    {
+        this.examList = loe;
+    }
+    
+
+    public List<Exam> getExamList() {
+        return examList;
+    }
+
+    public void setExamList(List<Exam> examList) {
+        this.examList = examList;
+    }
+    
+}
